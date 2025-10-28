e08e4970ff728fad3d7047e1f229e5559b6031df
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-10-28 15:12:40 +0400

    added main window setup
    -added title, window size, and made it not resizeable

--- Assessment 1 - Skills Portfolio/01-MathsQuiz/Maths-Quiz.py
+++ Assessment 1 - Skills Portfolio/01-MathsQuiz/Maths-Quiz.py
@@ -2,3 +2,11 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import itertools
+
+# setting up of main menu window
+root = Tk()
+root.title('Le Charne — Full Course Math Quiz')
+root.geometry('960x540')
+root.resizable(False, False)
+
+
