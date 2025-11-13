755186563cd93e37db4ebefb2d2c9140ec6a067c
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-11-13 19:18:40 +0400

    created class for the application
    indludes: def __init__() for setup and getting folder/directory of the script

--- Assessment 1 - Skills Portfolio/02-AlexaTellMeAJoke/Alexa-Jokes.py
+++ Assessment 1 - Skills Portfolio/02-AlexaTellMeAJoke/Alexa-Jokes.py
@@ -4,3 +4,15 @@ from tkinter import Label, Button
 from PIL import Image, ImageTk, ImageSequence
 import random
 import os
+
+# class for the application
+class AlexaJokeApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Alexa Joke Teller")
+        self.root.geometry("960x540")
+        self.root.resizable(False, False)
+        self.root.configure(bg="black")
+        
+        # get directory of the script
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
