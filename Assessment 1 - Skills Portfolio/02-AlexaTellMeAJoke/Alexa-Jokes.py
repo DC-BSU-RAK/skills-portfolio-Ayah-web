31610de43af9e2337db391bcf7bf82c4997128bc
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-11-13 19:25:01 +0400

    added self. code for loading jokes and gifs and to show the opening gif

--- Assessment 1 - Skills Portfolio/02-AlexaTellMeAJoke/Alexa-Jokes.py
+++ Assessment 1 - Skills Portfolio/02-AlexaTellMeAJoke/Alexa-Jokes.py
@@ -16,3 +16,27 @@ class AlexaJokeApp:
         
         # get directory of the script
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        # load jokes from file
+        self.jokes = self.load_jokes()
+        self.current_joke = None
+        self.current_setup = ""
+        self.current_punchline = ""
+        self.showing_punchline = False
+        # tracking if its the first joke
+        self.first_joke = True  
+        
+        # GIF frames storing
+        self.gif_frames = {}
+        self.current_gif_label = None
+        self.animation_id = None
+        self.current_frame_index = 0
+        
+        # store original button positions
+        self.button_positions = {}
+        
+        # load all GIFs
+        self.load_all_gifs()
+        
+        # show opening animation
+        self.show_opening_animation()
